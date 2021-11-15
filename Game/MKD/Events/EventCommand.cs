29a773b74a5adf3739b4036278f42e61eeae8269
Marijn Herrebout <m.k.herrebout@student.tudelft.nl>
2021-11-15 17:16:34 +0100

    Add enum for event command codes

--- Game/MKD/Events/EventCommand.cs
+++ Game/MKD/Events/EventCommand.cs
@@ -7,13 +7,13 @@ namespace RPGStudioMK.Game
     public class EventCommand
     {
         public int Indent;
-        public int Code;
+        public CommandCode Code;
         public List<object> Parameters = new List<object>();
 
         public EventCommand(IntPtr data)
         {
             this.Indent = (int) Ruby.Integer.FromPtr(Ruby.GetIVar(data, "@indent"));
-            this.Code = (int) Ruby.Integer.FromPtr(Ruby.GetIVar(data, "@code"));
+            this.Code = (CommandCode) Ruby.Integer.FromPtr(Ruby.GetIVar(data, "@code"));
             IntPtr parameters = Ruby.GetIVar(data, "@parameters");
             for (int i = 0; i < Ruby.Array.Length(parameters); i++)
             {
@@ -22,5 +22,93 @@ namespace RPGStudioMK.Game
                 this.Parameters.Add(obj);
             }
         }
+
+        public override string ToString()
+        {
+            return Code.ToString();
+        }
+    }
+
+    public enum CommandCode
+    {
+        Blank = 0,
+
+        ShowText = 101,
+        ShowCoices = 102,
+        InputNumber = 103,
+        ChangeTextOptions = 104,
+        Wait = 106,
+        Comment = 108,
+        ConditionalBranch = 111,
+        Loop = 112,
+        BreakLoop = 113,
+        ExitEventProcessing = 115,
+        EraseEvent = 116,
+        CallCommonEvent = 117,
+        Label = 118,
+        JumpToLabel = 119,
+        ControlSwitches = 121,
+        ControlVariables = 122,
+        ControlSelfSwitch = 123,
+        ControlTimer = 124,
+        ChangeMoney = 125,
+        ChangeWindowskin = 131,
+        ChangeBattleBGM = 132,
+        ChangeBattleEndME = 133,
+        ChangeSaveAccess = 134,
+        ChangeMenuAccess = 135,
+        ChangeEncounter = 136,
+
+        TransferPlayer = 201,
+        SetEventLocation = 202,
+        ScrollMap = 203,
+        ChangeMapSettings = 204,
+        ChangeFogColorTone = 205,
+        ChangeFogOpacity = 206,
+        ShowAnimation = 207,
+        ChangeTransparencyFlag = 208,
+        SetMoveRoute = 209,
+        WaitForMoveCompletion = 210,
+        PrepareForTransition = 221,
+        ExecuteTransition = 222,
+        ChangeScreenColorTone = 223,
+        ScreenFlash = 224,
+        ScreenShake = 225,
+        ShowPicture = 231,
+        MovePicture = 232,
+        RotatePicture = 233,
+        ChangePictureColorTone = 234,
+        ErasePicture = 235,
+        SetWeatherEffects = 236,
+        PlayBGM = 241,
+        FadeOutBGM = 242,
+        PlayBGS = 245,
+        FadeOutBGS = 246,
+        MemomorizeBGMBGS = 247,
+        RestoreBGMBGS = 238,
+        PlayME = 249,
+        PlaySE = 250,
+        StopSE = 251,
+
+        NameInputProcessing = 303,
+        HealAll = 314,
+        CallMenuScreen = 351,
+        CallSaveScreen = 352,
+        GameOver = 353,
+        ReturnToTitleScreen = 354,
+        Script = 355,
+
+        MoreText = 401,
+        BranchWhenXXX = 402,
+        BranchWhenCancel = 403,
+        BranchEnd = 404,
+        MoreComment= 408,
+        BranchElse = 411,
+        BranchConditionalEnd = 412,
+        RepeatAbove = 413,
+
+        MoreMoveRoute = 509,
+
+        MoreScript = 655
     }
 }
