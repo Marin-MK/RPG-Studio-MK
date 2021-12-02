623adb4f622307612120283911167de6dd5294be
Marijn Herrebout <m.k.herrebout@student.tudelft.nl>
2021-12-02 18:27:50 +0100

    Do not load tileset bitmap if graphic does not exist

--- Game/MKD/Tileset.cs
+++ Game/MKD/Tileset.cs
@@ -186,22 +186,26 @@ namespace RPGStudioMK.Game
             {
                 this.TilesetBitmap?.Dispose();
                 this.TilesetListBitmap?.Dispose();
-                
-                Bitmap bmp = new Bitmap($"{Data.ProjectPath}/Graphics/Tilesets/{this.GraphicName}.png");
-                this.TilesetBitmap = bmp;
-                int tileycount = (int) Math.Floor(bmp.Height / 32d);
 
-                this.TilesetListBitmap = new Bitmap(bmp.Width + 7, bmp.Height + tileycount - 1, Graphics.MaxTextureSize);
-                this.TilesetListBitmap.Unlock();
-
-                for (int tiley = 0; tiley < tileycount; tiley++)
+                string filename = $"{Data.ProjectPath}/Graphics/Tilesets/{this.GraphicName}.png";
+                if (Bitmap.FindRealFilename(filename) != null)
                 {
-                    for (int tilex = 0; tilex < 8; tilex++)
+                    Bitmap bmp = new Bitmap(filename);
+                    this.TilesetBitmap = bmp;
+                    int tileycount = (int) Math.Floor(bmp.Height / 32d);
+
+                    this.TilesetListBitmap = new Bitmap(bmp.Width + 7, bmp.Height + tileycount - 1, Graphics.MaxTextureSize);
+                    this.TilesetListBitmap.Unlock();
+
+                    for (int tiley = 0; tiley < tileycount; tiley++)
                     {
-                        this.TilesetListBitmap.Build(tilex * 32 + tilex, tiley * 32 + tiley, bmp, tilex * 32, tiley * 32, 32, 32);
+                        for (int tilex = 0; tilex < 8; tilex++)
+                        {
+                            this.TilesetListBitmap.Build(tilex * 32 + tilex, tiley * 32 + tiley, bmp, tilex * 32, tiley * 32, 32, 32);
+                        }
                     }
+                    this.TilesetListBitmap.Lock();
                 }
-                this.TilesetListBitmap.Lock();
             }
         }
 
