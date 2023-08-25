37593933c1bd2214eddc46e1389d414d4730ebe0
Marin-MK <m3rein@gmail.com>
2023-08-25 14:02:28 +0200

    Remove build files

--- build/build-win.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Building..."
-dotnet publish --runtime win-x64 --self-contained true --configuration Release -p:PublishSingleFile=true -p:PublishTrimmed=true
-Write-Host "Build complete."
-Start-Sleep -Seconds 2
