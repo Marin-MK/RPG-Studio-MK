46e49a9be8aba0b101954807bbc4657aea9faa80
Marin-MK <m3rein@gmail.com>
2023-09-02 02:12:31 +0200

    Improve SemanticVersion

--- src/util/SemanticVersion.cs
+++ src/util/SemanticVersion.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 
 namespace RPGStudioMK;
 
-[DebuggerDisplay("({Major}.{Minor}.{Patch})")]
-public struct SemanticVersion
+[DebuggerDisplay("({ToString()})")]
+public struct SemanticVersion : IComparable<SemanticVersion>
 {
     public int Major;
     public int Minor;
@@ -47,7 +48,28 @@ public struct SemanticVersion
         throw new ArgumentException($"Invalid version format (expected A.B.C, A.B, or A), got '{Version}'.");
     }
 
-	public override string ToString()
+    public int CompareTo(SemanticVersion other)
+    {
+        if (this.Major > other.Major) return 1;
+        if (this.Major < other.Major) return -1;
+        if (this.Minor > other.Minor) return 1;
+        if (this.Minor < other.Minor) return -1;
+        return this.Patch.CompareTo(other.Patch);
+    }
+
+    public override bool Equals([NotNullWhen(true)] object obj)
+    {
+        if (obj is not SemanticVersion) return false;
+        SemanticVersion v = (SemanticVersion) obj;
+        return this.Major == v.Major && this.Minor == v.Minor && this.Patch == v.Patch;
+    }
+
+    public override int GetHashCode()
+    {
+        return ToString().GetHashCode();
+    }
+
+    public override string ToString()
 	{
         if (this.Patch == 0)
         {
@@ -56,4 +78,13 @@ public struct SemanticVersion
         }
         return $"{this.Major}.{this.Minor}.{this.Patch}";
 	}
+
+    public static bool operator >(SemanticVersion a, SemanticVersion b) => a.CompareTo(b) == 1;
+    public static bool operator <(SemanticVersion a, SemanticVersion b) => a.CompareTo(b) == -1;
+    public static bool operator >=(SemanticVersion a, SemanticVersion b) => a.CompareTo(b) >= 0;
+    public static bool operator <=(SemanticVersion a, SemanticVersion b) => a.CompareTo(b) <= 0;
+    public static bool operator ==(SemanticVersion a, SemanticVersion b) => a.CompareTo(b) == 0;
+    public static bool operator !=(SemanticVersion a, SemanticVersion b) => a.CompareTo(b) != 0;
+
+    public static implicit operator SemanticVersion(string s) => new SemanticVersion(s);
 }
